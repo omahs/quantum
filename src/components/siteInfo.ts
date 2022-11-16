cded633b4fbf79a67c794396bb1830049fb9c24d
JJ Adonis <josejaimecareers@gmail.com>
2022-11-16 11:39:15 +0800

    chore(setup): bootstrap

---
+++ src/components/siteInfo.ts
@@ -0,0 +1,8 @@
+export const appName = "DeFiChain ERC-20 Bridge";
+export const shortDescription =
+  "DeFiChain";
+export const longDescription =
+  "DeFiChain";
+export const siteTitle = `DeFiChain ERC-20 Bridge`;
+export const website = "https://bridge.defichain.com/";
+export const logo = "/br_share.png";
