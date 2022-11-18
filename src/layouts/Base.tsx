894d3e2154ac89364cdc59edb61dc1b8754c72b7
Lyka <lykalabrada@gmail.com>
2022-11-18 10:56:48 +0800

    feature(ui-ux): update favicons

--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -70,12 +70,8 @@ function Base({ children }: PropsWithChildren<any>): JSX.Element | null {
         <meta name="twitter:card" content="summary_large_image" />
 
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
+        <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
       </Head>
 
       <WagmiConfig client={client}>
