5b7e134a03f037e8add653eea3dac9b6262821b0
Lyka <lykalabrada@gmail.com>
2022-11-19 19:02:54 +0800

    feature(ui-ux): update tw config to follow mono color list from DSM

--- src/components/ThemedText.tsx
+++ src/components/ThemedText.tsx
@@ -3,15 +3,16 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  color?: string;
   textStyle?: string;
 }
 
 export default function ThemedText({
   children,
+  color = "1000",
   textStyle = "",
 }: Props): JSX.Element {
   const isLight = false; // TODO: Get mode from context
-  const textColor = isLight ? "text-dark-900" : "text-light-50";
-
+  const textColor = isLight ? `text-light-${color}` : `text-dark-${color}`;
   return <span className={clsx(textColor, textStyle)}>{children}</span>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ function Home() {
   const { isMobile } = useResponsive();
   return (
     <section
-      className="relative min-h-screen flex flex-col bg-dark-900"
+      className="relative min-h-screen flex flex-col bg-dark-00"
       data-testid="homepage"
     >
       <Header />
-      <h1 className="text-light-50">
+      <h1 className="text-light-00">
         {isMobile
           ? "DeFiChain ERC-20 Bridge Mobile"
           : "DeFiChain ERC-20 Bridge"}
