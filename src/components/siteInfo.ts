2110f5cefad3ba973343f7b2ad6b61ca7b19719c
JJ Adonis <work@josejai.me>
2022-11-21 10:39:47 +0800

    chore(deps): bump packages

--- src/components/siteInfo.ts
+++ src/components/siteInfo.ts
@@ -1,7 +1,5 @@
 export const appName = "DeFiChain ERC-20 Bridge";
-export const shortDescription =
-  "DeFiChain";
-export const longDescription =
-  "DeFiChain";
+export const shortDescription = "DeFiChain";
+export const longDescription = "DeFiChain";
 export const siteTitle = `DeFiChain ERC-20 Bridge`;
 export const website = "https://bridge.defichain.com/";
