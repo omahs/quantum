3735a2ed0aad36d382a4723942996a48ab6d6bf5
Lyka <lykalabrada@gmail.com>
2022-11-23 14:06:08 +0800

    chore(test): add wallet input on homepage for review purposes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import WalletAddressInput from "@components/WalletAddressInput";
 import useResponsive from "@hooks/useResponsive";
 
 function Home() {
@@ -5,7 +6,7 @@ function Home() {
   const { isMobile } = useResponsive();
   return (
     <section
-      className="relative min-h-screen flex flex-col bg-dark-00"
+      className="relative min-h-screen flex flex-col bg-dark-00 p-2 lg:p-10 text-dark-1000"
       data-testid="homepage"
     >
       <h1 className="text-dark-1000">
@@ -13,6 +14,24 @@ function Home() {
           ? "DeFiChain ERC-20 Bridge Mobile"
           : "DeFiChain ERC-20 Bridge"}
       </h1>
+      {/* TODO: Remove test wallet input here */}
+      <div className="p-4 sm:w-1/2 border border-dark-100 rounded-lg">
+        Ethereum:
+        <WalletAddressInput blockchain="Ethereum" />
+        <br />
+        DeFiChain MainNet:
+        <WalletAddressInput blockchain="DeFiChain" />
+        <br />
+        DeFiChain TestNet:
+        <WalletAddressInput blockchain="DeFiChain" network="testnet" />
+        <br />
+        <br />
+        Ethereum Disabled:
+        <WalletAddressInput blockchain="Ethereum" network="testnet" disabled />
+        <br />
+        DeFiChain Disabled:
+        <WalletAddressInput blockchain="DeFiChain" network="testnet" disabled />
+      </div>
     </section>
   );
 }
