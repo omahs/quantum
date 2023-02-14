d974825d3ff2bd0f9ff8350052f1c9dc3ab59898
KevinK <31565174+kevzzsk@users.noreply.github.com>
2023-02-14 19:03:24 +0800

    feat(server): add health check service for database (#371)
    * EB-32 add health check service
    
    * fix deps
    
    * add test
    
    * resolve conflict
    
    * refactor into its own test file
    
    * refactor db failure test
    
    * fix missing container
    
    * fix lockfile
    
    * remove double stop container

---
+++ apps/server/src/HealthController.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck,HealthCheckService } from '@nestjs/terminus';
+
+import { PrismaHealthIndicator } from './health/PrismaHealth';
+
+@Controller('health')
+export class HealthController {
+  constructor(private health: HealthCheckService, private prisma: PrismaHealthIndicator) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.prisma.isHealthy('database')]);
+  }
+}
