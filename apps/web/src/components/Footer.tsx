1c84b3f0754615e3af8324f06f33a74a7f50f679
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-02-15 16:51:48 +0800

    code refactor

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { FaReddit, FaGithub, FaTwitter } from "react-icons/fa";
 import { BsMedium } from "react-icons/bs";
 import Image from "next/image";
+import Socials from "./commons/Socials";
 
 const DeFiChainSocialItems = [
   {
     icon: FaTwitter,
     testId: "twitter_dfc",
     label: "Twitter",
-    href: "https://twitter.com/BirthdayDev",
+    href: "https://twitter.com/defichain",
   },
   {
     icon: FaReddit,
@@ -48,15 +49,14 @@ export default function Footer() {
   return (
     <footer
       data-testid="footer"
-      className="bg-inherit relative z-[1] w-full border-dark-300 pb-[34px] pt-4 md:pt-3"
+      className="bg-inherit relative z-[1] w-full border-dark-300 pt-4 md:pt-3"
     >
-      {/* desktop view */}
       <section
         data-testid="footer_web"
-        className="text-dark-900 px-[120px] hidden lg:block"
+        className="text-dark-900 xs:px-[24px] xs:pb-[51px] md:px-[48px] lg:px-[120px] lg:pb-[34px] text-sm"
       >
-        <div className="border-t-[0.5px] dark:border-dark-300">
-          <div className="relative h-[32px] w-[140px] cursor-pointer lg:h-[60px] lg:w-[264px] mt-3">
+        <div className="border-t-[0.5px] dark:border-dark-300 xs:pb-[17.5px] pt-[15px]">
+          <div className="relative h-[28px] w-[250px] cursor-pointer mt-3">
             <Image
               fill
               data-testid="footer-bridge-logo"
@@ -65,115 +65,44 @@ export default function Footer() {
             />
           </div>
         </div>
-        <div className="pl-1 flex flex-row justify-between text-sm">
-          <div className="flex flex-col">
-            <div className="font-semibold">
-              Quantum is a proud development of Birthday Research — the
-              blockchain R&D arm of Cake DeFi.
+        {/* Desktop view */}
+        <div className="pl-1 flex-row justify-between lg:block hidden">
+          <div className="font-semibold">
+            Quantum is a proud development of Birthday Research — the blockchain
+            R&D arm of Cake DeFi.
+          </div>
+          <div className="flex flex-row items-center justify-between pt-[26px]">
+            <div className="flex flex-row">
+              <div className="pr-[11px]">&copy; Birthday Research</div>
+              <Socials items={BirthdayResearchSocialItems} />
             </div>
-            <div className="md:items-center flex flex-row">
-              <div className="flex flex-row justify-between pt-2.5 pb-2 pr-[12.5px]">
+            <div className="flex flex-row-reverse">
+              <div className="pl-[11px]">&copy; DeFiChain</div>
+              <Socials items={DeFiChainSocialItems} />
+            </div>
+          </div>
+        </div>
+        {/* Tablet and Mobile view */}
+        <div className="pl-1 lg:hidden">
+          <div className="font-semibold">
+            Quantum is a proud development of Birthday Research — the blockchain
+            R&D arm of Cake DeFi.
+          </div>
+          <div className="flex flex-row pt-[26px] justify-between">
+            <div className="flex xs:flex-col md:flex-row">
+              <div className="flex flex-row justify-between pb-2 md:pr-[11px]">
                 &copy; Birthday Research
               </div>
-              <div
-                data-testid="br_socials"
-                className="flex flex-row space-x-3.5"
-              >
-                {BirthdayResearchSocialItems.map(
-                  ({ href, testId, icon: Icon }) => (
-                    <a
-                      href={href}
-                      key={testId}
-                      target="_blank"
-                      className=""
-                      rel="noreferrer"
-                      data-testid={testId}
-                    >
-                      <Icon size={18} />
-                    </a>
-                  )
-                )}
+              <div className="flex flex-row md:space-x-3">
+                <Socials items={BirthdayResearchSocialItems} />
               </div>
             </div>
-          </div>
-          <div className="flex-col self-end relative bottom-[10px]">
-            <div className="pb-2">&copy; DeFiChain</div>
-            <div className="flex flex-row justify-between">
-              {DeFiChainSocialItems.map(({ href, testId, icon: Icon }) => (
-                <a
-                  href={href}
-                  key={testId}
-                  target="_blank"
-                  rel="noreferrer"
-                  className=""
-                  data-testid={testId}
-                >
-                  <Icon size={18} />
-                </a>
-              ))}
+            <div className="flex xs:flex-col-reverse md:flex-row">
+              <div className="flex flex-row justify-between pr-[11px] xs:space-x-3.5 xs:pt-2 md:pt-0">
+                <Socials items={DeFiChainSocialItems} />
+              </div>
+              <div>&copy; DeFiChain</div>
             </div>
-          </div>
-        </div>
-      </section>
-
-      {/* tablet and mobile view */}
-      <section
-        data-testid="footer_tablet_mobile"
-        className="px-10 text-dark-900 text-xs lg:hidden"
-      >
-        <div className="border-t-[0.5px] dark:border-dark-300">
-          <div className="relative h-[32px] w-[140px] cursor-pointer my-6 lg:h-[60px] lg:w-[264px]">
-            <Image
-              fill
-              data-testid="footer-bridge-logo"
-              src="/header-tablet-mobile-logo.svg"
-              alt="Bridge Logo"
-            />
-          </div>
-        </div>
-        <div className="font-semibold">Version 1.0.1&nbsp;</div>
-        <div className="font-semibold">
-          Quantum is a proud development of Birthday Research — the blockchain
-          R&D arm of Cake DeFi.
-        </div>
-        <div className="flex flex-row pt-[26px] justify-between">
-          <div className="flex xs:flex-col md:flex-row">
-            <div className="flex flex-row justify-between pb-2 pr-[12.5px] md:pb-0">
-              &copy; Birthday Research
-            </div>
-            <div className="flex flex-row space-x-3">
-              {BirthdayResearchSocialItems.map(
-                ({ href, testId, icon: Icon }) => (
-                  <a
-                    href={href}
-                    key={testId}
-                    target="_blank"
-                    rel="noreferrer"
-                    className=""
-                    data-testid={`${testId}_tablet_mobile`}
-                  >
-                    <Icon size={18} />
-                  </a>
-                )
-              )}
-            </div>
-          </div>
-          <div className="flex xs:flex-col-reverse md:flex-row">
-            <div className="flex flex-row justify-between pr-[11px] xs:space-x-3.5 xs:pt-2 md:pt-0">
-              {DeFiChainSocialItems.map(({ href, testId, icon: Icon }) => (
-                <a
-                  href={href}
-                  key={testId}
-                  target="_blank"
-                  rel="noreferrer"
-                  className=""
-                  data-testid={testId}
-                >
-                  <Icon size={18} />
-                </a>
-              ))}
-            </div>
-            <div>&copy; DeFiChain</div>
           </div>
         </div>
       </section>
