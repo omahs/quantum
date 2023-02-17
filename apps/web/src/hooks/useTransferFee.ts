dd1793dce1a2e5068d99e6d232006203fe94f137
Julio Caesar <leo.caesar@live.com>
2023-02-17 18:06:07 +0800

    feature(core): transaction fees adjustment  (#436)
    * adjust transfer fees
    
    * fee adjustment

--- apps/web/src/hooks/useTransferFee.ts
+++ apps/web/src/hooks/useTransferFee.ts
@@ -1,15 +1,24 @@
 import BigNumber from "bignumber.js";
 import { useNetworkContext } from "@contexts/NetworkContext";
+import { Network } from "types";
+import { ETHEREUM_SYMBOL } from "../constants";
 
 /**
  * Computes transfer fee
  * Any changes to the fee logic can be updated here
  */
-const FEE_PERCENTAGE = 0.003;
+
+const EVM_TO_DFC_FEE_PERCENTAGE = 0.001;
+const DFC_TO_EVM_FEE_PERCENTAGE = 0.003;
 export default function useTransferFee(transferAmount: string | number) {
-  const { selectedTokensA } = useNetworkContext();
+  const { selectedNetworkA, selectedTokensA } = useNetworkContext();
+  const isSendingErcToken =
+    selectedNetworkA.name === Network.Ethereum &&
+    selectedTokensA.tokenA.name !== ETHEREUM_SYMBOL;
   const feeSymbol = selectedTokensA.tokenA.name;
-  const fee = new BigNumber(transferAmount || 0).multipliedBy(FEE_PERCENTAGE);
+  const fee = new BigNumber(transferAmount || 0).multipliedBy(
+    isSendingErcToken ? EVM_TO_DFC_FEE_PERCENTAGE : DFC_TO_EVM_FEE_PERCENTAGE
+  );
 
   return [fee, feeSymbol];
 }
