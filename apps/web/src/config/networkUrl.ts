058239b0e05841aa1d4b2ff41bcaebee8ace4c6e
KevinK <kevin.kihata@gmail.com>
2023-02-17 14:49:05 +0800

    fix localhost url + todo

--- apps/web/src/config/networkUrl.ts
+++ apps/web/src/config/networkUrl.ts
@@ -1,11 +1,12 @@
 import { EnvironmentNetwork } from "@waveshq/walletkit-core";
 
+// TODO: Replace URLs with real URLs
 const BASE_URLS: { [key in EnvironmentNetwork]: string } = {
-  [EnvironmentNetwork.LocalPlayground]: "http://localhost:5741",
-  [EnvironmentNetwork.RemotePlayground]: "http://localhost:5741",
-  [EnvironmentNetwork.TestNet]: "http://localhost:5741",
-  [EnvironmentNetwork.DevNet]: "http://localhost:5741",
-  [EnvironmentNetwork.MainNet]: "http://localhost:5741",
+  [EnvironmentNetwork.LocalPlayground]: "http://localhost:5741/defichain",
+  [EnvironmentNetwork.RemotePlayground]: "http://localhost:5741/defichain",
+  [EnvironmentNetwork.TestNet]: "http://localhost:5741/defichain",
+  [EnvironmentNetwork.DevNet]: "http://localhost:5741/defichain",
+  [EnvironmentNetwork.MainNet]: "http://localhost:5741/defichain",
 };
 
 export default BASE_URLS;
