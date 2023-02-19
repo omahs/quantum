e0f9a0cb1513fc75c1f0fab9ef10515b72374360
Lyka Labrada <lykalabrada@gmail.com>
2023-02-19 12:58:13 +0800

    feat(server): signclaim - set deadline to 24hours after creation (#420)
    * feat(server): signclaim - set deadline to 24hours after creation
    
    * feat(server): remove unnecessary math function
    
    ---------
    
    Co-authored-by: thedoublejay <work@josejai.me>

--- apps/server/src/utils/DateUtils.ts
+++ apps/server/src/utils/DateUtils.ts
@@ -1,7 +1,5 @@
 export function getNextDayTimestamp(): number {
   const duration = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
-  const currentTimestamp = Math.floor(Date.now());
-  const nextDayTimestamp = currentTimestamp + duration;
 
-  return nextDayTimestamp;
+  return Date.now() + duration;
 }
