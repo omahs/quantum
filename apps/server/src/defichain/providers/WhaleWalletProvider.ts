554d74f4184c3fca8e79ea1844383a98a9102b17
JJ Adonis <work@josejai.me>
2023-03-02 17:42:11 +0800

    fix(server): prevent creation of defichain private keys if it's not 2… (#634)
    * fix(server): prevent creation of defichain private keys if it's not 24 words
    
    * fix(server): use validateMnemonic from Jellyfish
    
    * update docker conflict
    
    * fix breaking tests
    
    * fix breaking tests

--- apps/server/src/defichain/providers/WhaleWalletProvider.ts
+++ apps/server/src/defichain/providers/WhaleWalletProvider.ts
@@ -1,5 +1,9 @@
 import { JellyfishWallet, WalletHdNode, WalletHdNodeProvider } from '@defichain/jellyfish-wallet';
-import { MnemonicHdNodeProvider, MnemonicProviderData } from '@defichain/jellyfish-wallet-mnemonic';
+import {
+  MnemonicHdNodeProvider,
+  MnemonicProviderData,
+  validateMnemonicSentence,
+} from '@defichain/jellyfish-wallet-mnemonic';
 import { WhaleWalletAccount, WhaleWalletAccountProvider } from '@defichain/whale-api-wallet';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -18,6 +22,9 @@ export class WhaleWalletProvider {
 
   createWallet(index: number = 2): WhaleWalletAccount {
     const mnemonic = this.configService.getOrThrow<string>(`defichain.key`);
+    if (!validateMnemonicSentence(mnemonic)) {
+      throw new Error('Invalid DeFiChain private keys!');
+    }
     const data = this.toData(mnemonic.split(' '), this.network);
     const provider = this.initProvider(data, this.network);
     return this.initJellyfishWallet(provider, this.network).get(index);
