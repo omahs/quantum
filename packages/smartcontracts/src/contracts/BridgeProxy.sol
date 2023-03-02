169fcdfd055806130b02f7514c274929f5b406b9
cuongquangnam <tccuong1999@gmail.com>
2023-03-02 11:22:42 +0800

    change Solidity versions in smart contracts

--- packages/smartcontracts/src/contracts/BridgeProxy.sol
+++ packages/smartcontracts/src/contracts/BridgeProxy.sol
@@ -1,3 +1,4 @@
+pragma solidity 0.8.17;
 import '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol';
 
 contract BridgeProxy is ERC1967Proxy {
--- packages/smartcontracts/src/contracts/BridgeV1.sol
+++ packages/smartcontracts/src/contracts/BridgeV1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
