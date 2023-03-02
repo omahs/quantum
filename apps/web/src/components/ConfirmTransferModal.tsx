dcabf80d5f7abe90dd7377fc8b559500ba521950
G9000 <leo.caesar@live.com>
2023-03-02 19:13:23 +0800

    code cleanup

--- apps/web/src/components/ConfirmTransferModal.tsx
+++ apps/web/src/components/ConfirmTransferModal.tsx
@@ -146,7 +146,7 @@ export default function ConfirmTransferModal({
       tokenName: selectedTokensB.tokenA.name,
       tokenSymbol: selectedTokensB.tokenA.symbol,
       tokenIcon: selectedTokensB.tokenA.icon,
-      amount: new BigNumber(amount),
+      amount: new BigNumber(amount).minus(fee),
     },
   };
 
