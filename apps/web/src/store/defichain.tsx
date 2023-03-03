934b0188a27a2fb101155797d6e7e00904493e9f
Lyka Labrada <lykalabrada@gmail.com>
2023-03-03 11:10:14 +0800

    feat(server): add `/settings` api for settings shared with frontend (#637)
    * feat(server): create api to return shared settings with client
    
    * feat(ui-ux): add lazy query for settings
    
    * feat(ui-ux): get txn fee from settings api

--- apps/web/src/store/defichain.tsx
+++ apps/web/src/store/defichain.tsx
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { FetchArgs } from "@reduxjs/toolkit/dist/query/fetchBaseQuery";
-import { AddressDetails, BridgeVersion } from "types";
+import { AddressDetails, BridgeSettings, BridgeVersion } from "types";
 import { HttpStatusCode } from "axios";
 
 const staggeredBaseQueryWithBailOut = retry(
@@ -106,6 +106,12 @@ export const bridgeApi = createApi({
     bridgeVersion: builder.query<BridgeVersion, any>({
       query: ({ baseUrl }) => ({
         url: `${baseUrl}/version`,
+        method: "GET",
+      }),
+    }),
+    bridgeSettings: builder.query<BridgeSettings, any>({
+      query: ({ baseUrl }) => ({
+        url: `${baseUrl}/settings`,
         method: "GET",
       }),
     }),
