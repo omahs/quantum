e3d76ceac0975c828c957f2f4f21af77385de0c0
Lyka Labrada <lykalabrada@gmail.com>
2023-03-06 11:06:33 +0800

    feat(ui-ux): hide network switch when on mainnet ETH

--- apps/web/src/layouts/contexts/NetworkEnvironmentContext.tsx
+++ apps/web/src/layouts/contexts/NetworkEnvironmentContext.tsx
@@ -7,7 +7,9 @@ import React, {
   PropsWithChildren,
 } from "react";
 import { useRouter } from "next/router";
+import { useNetwork } from "wagmi";
 import { EnvironmentNetwork, getEnvironment } from "@waveshq/walletkit-core";
+import { ETHEREUM_MAINNET_ID } from "../../constants";
 
 interface NetworkContextI {
   networkEnv: EnvironmentNetwork;
@@ -30,8 +32,11 @@ export function NetworkEnvironmentProvider({
   const env = getEnvironment(process.env.NODE_ENV);
   const networkQuery = router.query.network;
 
+  const { chain } = useNetwork();
+  const isEthereumMainNet = chain?.id === ETHEREUM_MAINNET_ID;
+
   function getNetwork(n: EnvironmentNetwork): EnvironmentNetwork {
-    if (env.networks.includes(n)) {
+    if (!isEthereumMainNet && env.networks.includes(n)) {
       return n;
     }
     return EnvironmentNetwork.MainNet;
@@ -42,6 +47,10 @@ export function NetworkEnvironmentProvider({
     useState<EnvironmentNetwork>(initialNetwork);
 
   const handleNetworkEnvChange = (value: EnvironmentNetwork) => {
+    if (isEthereumMainNet) {
+      // Network environment should never be updated
+      return;
+    }
     setNetworkEnv(value);
     if (value !== initialNetwork) {
       router.replace(
