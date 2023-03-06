3da95dd8d1f07610601beb6da83fd8e79829b4d4
Harsh <harshrathi.dev@gmail.com>
2023-03-06 10:43:53 +0530

    fix(ui-ux): aligned X and Close buttons in Transaction error modal

--- apps/web/src/components/commons/ErrorModal.tsx
+++ apps/web/src/components/commons/ErrorModal.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { FiAlertCircle } from "react-icons/fi";
 import ActionButton from "./ActionButton";
 import Modal from "./Modal";
@@ -16,7 +15,6 @@ export default function ErrorModal({
   onPrimaryButtonClick?: () => void;
   onClose: () => void;
 }): JSX.Element {
-  const router = useRouter();
   return (
     <Modal isOpen onClose={onClose}>
       <div className="flex flex-col items-center mt-6 mb-14">
@@ -35,7 +33,7 @@ export default function ErrorModal({
             label="Close"
             variant="secondary"
             customStyle="mt-2 md:px-2.5 lg:text-xl lg:leading-8 lg:py-2 lg:px-8 xl:px-14"
-            onClick={() => router.reload()}
+            onClick={onClose}
           />
         </span>
       </div>
