f4e83175a8fe74f498d6abb07b603b5acfc2c499
JJ Adonis <work@josejai.me>
2023-03-07 18:44:21 +0800

    fix(server): update wbtc mapping

--- apps/web/src/layouts/contexts/NetworkContext.tsx
+++ apps/web/src/layouts/contexts/NetworkContext.tsx
@@ -44,7 +44,7 @@ export const networks: [NetworkI<Erc20Token>, NetworkI<string>] = [
     tokens: [
       {
         tokenA: {
-          name: "wBTC",
+          name: "WBTC",
           symbol: "WBTC",
           icon: "/tokens/wBTC.svg",
           supply: "1925543.1234",
@@ -84,7 +84,7 @@ export const networks: [NetworkI<Erc20Token>, NetworkI<string>] = [
           supply: "1801245.4321",
         },
         tokenB: {
-          name: "wBTC",
+          name: "WBTC",
           symbol: "WBTC",
           icon: "/tokens/wBTC.svg",
           supply: "1801245.4321",
