3d12e24507589d7de93ece181ce65fb945799629
nattadex <elocinnat99@gmail.com>
2023-03-14 14:15:12 +0800

    used SemaphoreCache after discussion

--- apps/server/src/defichain/controllers/StatsController.ts
+++ apps/server/src/defichain/controllers/StatsController.ts
@@ -1,25 +1,39 @@
 // import { stats } from '@defichain/whale-api-client';
-import { CacheInterceptor, CacheTTL, Controller, Get, Query, UseInterceptors } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { SkipThrottle } from '@nestjs/throttler';
-import { EnvironmentNetwork } from '@waveshq/walletkit-core';
+import { SemaphoreCache } from 'src/libs/caches/SemaphoreCache';
 
 import { DeFiChainStats } from '../DefichainInterface';
 import { DeFiChainStatsService } from '../services/DeFiChainStatsService';
 
-@UseInterceptors(CacheInterceptor)
 @Controller()
 export class StatsController {
-  private network: EnvironmentNetwork;
-
-  constructor(private readonly configService: ConfigService, private defichainStatsService: DeFiChainStatsService) {
-    this.network = configService.getOrThrow<EnvironmentNetwork>(`defichain.network`);
-  }
+  constructor(protected readonly cache: SemaphoreCache, private defichainStatsService: DeFiChainStatsService) {}
 
   @SkipThrottle()
-  @CacheTTL(3600_000 * 24) // 1 day
   @Get('/stats')
   async get(@Query('date') date?: string): Promise<DeFiChainStats> {
-    return this.defichainStatsService.getDefiChainStats(date);
+    return (await this.cache.get(
+      `DFC_STATS_${date ?? 'TODAY'}`,
+      async () => {
+        try {
+          return await this.defichainStatsService.getDefiChainStats(date);
+        } catch (e: any) {
+          throw new HttpException(
+            {
+              status: e.code || HttpStatus.INTERNAL_SERVER_ERROR,
+              error: `API call for DefiChain statistics was unsuccessful: ${e.message}`,
+            },
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            {
+              cause: e,
+            },
+          );
+        }
+      },
+      {
+        ttl: 3600_000 * 24, // 1 day
+      },
+    )) as DeFiChainStats;
   }
 }
