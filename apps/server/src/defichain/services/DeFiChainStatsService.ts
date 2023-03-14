2a3caa62534c349ed713041db05c3e46d889503e
nattadex <elocinnat99@gmail.com>
2023-03-14 14:15:30 +0800

    clean ups

--- apps/server/src/defichain/services/DeFiChainStatsService.ts
+++ apps/server/src/defichain/services/DeFiChainStatsService.ts
@@ -17,61 +17,35 @@ export class DeFiChainStatsService {
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1);
 
-    const confirmedTransactions = await this.prisma.deFiChainAddressIndex.findMany({
-      where: {
-        claimSignature: { not: null },
-        address: { not: undefined },
-        createdAt: {
-          // new Date() creates date with current time and day and etc.
-          gte: today,
-          lt: tomorrow,
+    const [totalTransactions, confirmedTransactions] = await Promise.all([
+      this.prisma.deFiChainAddressIndex.count({
+        where: {
+          createdAt: {
+            //   new Date() creates date with current time and day and etc.
+            gte: today,
+            lt: tomorrow,
+          },
         },
-      },
-    });
+      }),
 
-    const totalTransactions = await this.prisma.deFiChainAddressIndex.count({
-      where: {
-        createdAt: {
-          //   new Date() creates date with current time and day and etc.
-          gte: today,
-          lt: tomorrow,
+      this.prisma.deFiChainAddressIndex.findMany({
+        where: {
+          claimSignature: { not: null },
+          address: { not: undefined },
+          createdAt: {
+            // new Date() creates date with current time and day and etc.
+            gte: today,
+            lt: tomorrow,
+          },
         },
-      },
-    });
+        select: {
+          tokenSymbol: true,
+          claimAmount: true,
+        },
+      }),
+    ]);
 
-    const amountBridgedBigN = {
-      USDC: BigNumber(0),
-      USDT: BigNumber(0),
-      BTC: BigNumber(0),
-      ETH: BigNumber(0),
-      DFI: BigNumber(0),
-      EUROC: BigNumber(0),
-    };
-    for (const transaction of confirmedTransactions) {
-      const { tokenSymbol, claimAmount } = transaction;
-      if (tokenSymbol && claimAmount !== null) {
-        amountBridgedBigN[tokenSymbol as SupportedDFCTokenSymbols] = amountBridgedBigN[
-          tokenSymbol as SupportedDFCTokenSymbols
-        ].plus(BigNumber(claimAmount as string));
-      }
-    }
-    const numericalPlaceholder = '0.000000';
-    const amountBridgedToDfc: BridgedEvmToDfc = {
-      USDC: numericalPlaceholder,
-      USDT: numericalPlaceholder,
-      BTC: numericalPlaceholder,
-      ETH: numericalPlaceholder,
-      DFI: numericalPlaceholder,
-      EUROC: numericalPlaceholder,
-    };
-
-    for (const key in amountBridgedBigN) {
-      if ({}.hasOwnProperty.call(amountBridgedBigN, key)) {
-        amountBridgedToDfc[key as SupportedDFCTokenSymbols] = amountBridgedBigN[key as SupportedDFCTokenSymbols]
-          .decimalPlaces(6, BigNumber.ROUND_FLOOR)
-          .toString();
-      }
-    }
+    const amountBridgedToDfc = getAmountBridged(confirmedTransactions);
 
     return {
       totalTransactions,
@@ -79,4 +53,47 @@ export class DeFiChainStatsService {
       amountBridgedToDfc,
     };
   }
+}
+
+function getAmountBridged(
+  confirmedTransactions: Array<{
+    tokenSymbol: string | null;
+    claimAmount: string | null;
+  }>,
+): BridgedEvmToDfc {
+  const amountBridgedBigN = {
+    USDC: BigNumber(0),
+    USDT: BigNumber(0),
+    BTC: BigNumber(0),
+    ETH: BigNumber(0),
+    DFI: BigNumber(0),
+    EUROC: BigNumber(0),
+  };
+  for (const transaction of confirmedTransactions) {
+    const { tokenSymbol, claimAmount } = transaction;
+    if (tokenSymbol && claimAmount !== null) {
+      amountBridgedBigN[tokenSymbol as SupportedDFCTokenSymbols] = amountBridgedBigN[
+        tokenSymbol as SupportedDFCTokenSymbols
+      ].plus(BigNumber(claimAmount as string));
+    }
+  }
+  const numericalPlaceholder = '0.000000';
+  const amountBridgedToDfc: BridgedEvmToDfc = {
+    USDC: numericalPlaceholder,
+    USDT: numericalPlaceholder,
+    BTC: numericalPlaceholder,
+    ETH: numericalPlaceholder,
+    DFI: numericalPlaceholder,
+    EUROC: numericalPlaceholder,
+  };
+
+  for (const key in amountBridgedBigN) {
+    if ({}.hasOwnProperty.call(amountBridgedBigN, key)) {
+      amountBridgedToDfc[key as SupportedDFCTokenSymbols] = amountBridgedBigN[key as SupportedDFCTokenSymbols]
+        .decimalPlaces(6, BigNumber.ROUND_FLOOR)
+        .toString();
+    }
+  }
+
+  return amountBridgedToDfc;
 }
