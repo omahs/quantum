4e472122e2d4a8414c7a0aada8e1648e3cf44316
nattadex <elocinnat99@gmail.com>
2023-03-15 14:54:56 +0800

    moved dto to interface

--- apps/server/src/defichain/DefichainInterface.ts
+++ apps/server/src/defichain/DefichainInterface.ts
@@ -1,3 +1,4 @@
+import { IsDateString, IsOptional } from 'class-validator';
 import { SupportedDFCTokenSymbols } from 'src/AppConfig';
 
 export interface DeFiChainStats {
@@ -6,3 +7,25 @@ export interface DeFiChainStats {
   amountBridged: BridgedEvmToDfc;
 }
 export type BridgedEvmToDfc = Record<SupportedDFCTokenSymbols, string>;
+
+export type Iso8601DateOnlyString = `${number}-${number}-${number}`;
+
+export class StatsQueryDto {
+  @IsDateString()
+  @IsOptional()
+  date?: Iso8601DateOnlyString;
+}
+
+export class StatsDto {
+  readonly totalTransactions: number;
+
+  readonly confirmedTransactions: number;
+
+  readonly amountBridged: BridgedEvmToDfc;
+
+  constructor(statsModel: DeFiChainStats) {
+    this.totalTransactions = statsModel.totalTransactions;
+    this.confirmedTransactions = statsModel.confirmedTransactions;
+    this.amountBridged = statsModel.amountBridged;
+  }
+}
--- apps/server/src/defichain/model/StatsDto.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsDate, IsOptional } from 'class-validator';
-
-export class StatsDto {
-  @Type(() => Date)
-  @IsDate()
-  @IsOptional()
-  date?: Date;
-}
