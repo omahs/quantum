65cf601a29bb63bf18bf90a627eb35f68e9b0bbb
nattadex <elocinnat99@gmail.com>
2023-03-16 10:52:51 +0800

    kept interface format consistent

--- apps/server/src/defichain/DefichainInterface.ts
+++ apps/server/src/defichain/DefichainInterface.ts
@@ -3,11 +3,27 @@ import { SupportedDFCTokenSymbols } from 'src/AppConfig';
 
 import { Iso8601DateOnlyString } from '../utils/StatsUtils';
 
-export interface DeFiChainStats {
-  totalTransactions: number;
-  confirmedTransactions: number;
-  amountBridged: BridgedDfcToEvm;
+export class DeFiChainStats {
+  readonly totalTransactions: number;
+
+  readonly confirmedTransactions: number;
+
+  readonly amountBridged: BridgedDfcToEvm;
+
+  constructor(totalTransactions: number, confirmedTransactions: number, amountBridged: BridgedDfcToEvm) {
+    this.totalTransactions = totalTransactions;
+    this.confirmedTransactions = confirmedTransactions;
+    this.amountBridged = {
+      USDC: amountBridged[SupportedDFCTokenSymbols.USDC]?.toString(),
+      USDT: amountBridged[SupportedDFCTokenSymbols.USDT]?.toString(),
+      BTC: amountBridged[SupportedDFCTokenSymbols.BTC]?.toString(),
+      ETH: amountBridged[SupportedDFCTokenSymbols.ETH]?.toString(),
+      DFI: amountBridged[SupportedDFCTokenSymbols.DFI]?.toString(),
+      EUROC: amountBridged[SupportedDFCTokenSymbols.EUROC]?.toString(),
+    };
+  }
 }
+
 export type BridgedDfcToEvm = Record<SupportedDFCTokenSymbols, string>;
 
 export class DFCStatsDto {
